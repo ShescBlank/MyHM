40e3848b9bf874cd14ca06c6c0d8fd88f03673d4
ShescBlank <alberto.almuna@uc.cl>
2024-12-30 14:22:29 -0300

    Modifications to the ACAs and their stop criteria

--- MyHM/compression/aca.py
+++ MyHM/compression/aca.py
@@ -1,19 +1,15 @@
 import numpy as _np
 from scipy.sparse import lil_matrix
 
-# TODO: En teoría puedo limpiar un poco los códigos, como quitar las listas I y J, sacar el first_abs_delta. Y decidir que hacer con la parte singular
-
 # https://www.epfl.ch/labs/anchp/wp-content/uploads/2018/10/lecture4-slides.pdf
 # https://web.archive.org/web/20060903235945id_/http://esl.eng.ohio-state.edu/~csg/papers/79.pdf
-def ACAPP(A, epsilon = 0.1, verbose=False):
+def ACAPP(A, epsilon = 0.1, exact_error=False):
+    # Saqué variables extras, saqué I,J, puse -1 en los abs, quité los copy y cambié una de las condiciones de salida
     # R = _np.copy(A)
     R = A
     m, n = R.shape
-    assembled_values = lil_matrix((m, n), dtype=R.dtype)
     mask_row = _np.zeros(n, dtype=bool)
     mask_col = _np.zeros(m, dtype=bool)
-    I = []
-    J = []
     k = 0
     i_star = 0
 
@@ -22,7 +18,7 @@ def ACAPP(A, epsilon = 0.1, verbose=False):
 
     # Stopping criterion:
     sum_uv_norm_square = 0
-    first_abs_delta = 0
+    norm_A = _np.linalg.norm(A)
     
     while True:
         aux = 0
@@ -32,40 +28,29 @@ def ACAPP(A, epsilon = 0.1, verbose=False):
             aux = _np.asarray(u_vectors)[:, i_star].T @ _np.asarray(v_vectors)
 
         # Row of original matrix:
-        R_row = R[i_star, :] - aux # Old version
-        if mask_col[i_star] != True and _np.sum(~mask_row) > 0:
-        #     assembled_values[i_star, ~mask_row] = R[i_star, ~mask_row]
-            mask_col[i_star] = True
-        # R_row = assembled_values[i_star, :].toarray().flatten() - aux
+        R_row = R[i_star, :] - aux # Resto la aproximación que llevamos de la fila original
+        mask_col[i_star] = True
 
-        R_row_copy = _np.copy(R_row)
-        R_row_copy[J] = 0
-        j_star = _np.argmax(_np.abs(R_row_copy))
-        delta = R_row_copy[j_star]
-        if first_abs_delta == 0:
-            first_abs_delta = abs(delta)
+        R_row_abs = _np.abs(R_row)
+        R_row_abs[mask_row] = -1
+        j_star = _np.argmax(R_row_abs)
+        assert R_row_abs[j_star] != -1, "-1 found in R_row_abs"
+        if R_row_abs[j_star] == -1: # En teoría esto nunca debería ocurrir (por la condición de salida más abajo)
+            delta = 0
+        else:
+            delta = R_row[j_star]
 
-        # if delta == 0 or abs(delta)/first_abs_delta <= 1e-15: # Agregamos un pequeño margen (puede ser complejo)
         if delta == 0:
-            if _np.sum(mask_col) >= min(m, n):
+            if _np.sum(mask_col) == m: # OJO: acá tenía min(m,n), pero eso no es correcto
                 if len(u_vectors) == 0:
-                    u_vectors.append(_np.zeros(m, dtype=R.dtype)) # 1xm
-                    v_vectors.append(_np.zeros(n, dtype=R.dtype)) # 1xn
+                    if m + n <= m * n:
+                        u_vectors.append(_np.zeros(m, dtype=R.dtype)) # 1xm
+                        v_vectors.append(_np.zeros(n, dtype=R.dtype)) # 1xn
+                    else:
+                        return A, None # Old version
                 break
             i_star = _np.random.choice(_np.arange(m)[~mask_col])
             continue
-            # ===========
-            # if len(I) >= min(m, n): # Para que no siga agregando en caso de ya tener todos los índices
-            #     # print("Not reached Epsilon")
-            #     exit_code = 0
-            #     break
-            # if len(u_vectors) == 0: # Edge case in which the first row chosen consists of zeros only
-            #     i_star += 1
-            #     if i_star == m: # Everything is zero
-            #         u_vectors.append(_np.zeros(m, dtype=R.dtype)) # 1xm
-            #         v_vectors.append(_np.zeros(n, dtype=R.dtype)) # 1xn
-            #         break
-            #     continue
         else:
             v = R_row / delta
             aux = 0
@@ -76,28 +61,18 @@ def ACAPP(A, epsilon = 0.1, verbose=False):
 
             # Column of original matrix:
             u = R[:, j_star] - aux # Old version
-            if mask_row[j_star] != True and _np.sum(~mask_col) > 0:
-            #     assembled_values[~mask_col, j_star] = R[~mask_col, j_star]
-                mask_row[j_star] = True
-            # u = assembled_values[:, j_star].toarray().flatten() - aux
+            mask_row[j_star] = True
 
             k += 1
             u_vectors.append(u)
             v_vectors.append(v)
-        I.append(i_star)
-        J.append(j_star)
-        u_copy = _np.copy(u_vectors[-1])
-        u_copy[I] = 0
-        i_star = _np.argmax(_np.abs(u_copy))
+        u_abs = _np.abs(u_vectors[-1])
+        u_abs[mask_col] = -1
+        i_star = _np.argmax(u_abs)
 
         # Check if compression is still viable:
-        if len(u_vectors) * len(u_vectors[0]) + len(v_vectors) * len(v_vectors[0]) > m * n:
+        if len(u_vectors) * m + len(v_vectors) * n > m * n:
             return A, None # Old version
-            # matrix = assembled_values.toarray()
-            # if _np.sum(~mask_col) > 0 and _np.sum(~mask_row) > 0:
-            #     meshgrid = _np.meshgrid(_np.arange(m)[~mask_col], _np.arange(n)[~mask_row], indexing="ij")
-            #     matrix[meshgrid[0], meshgrid[1]] = R[meshgrid[0], meshgrid[1]]
-            # return matrix, None
 
         # Stopping criterion:
         u = u_vectors[-1]
@@ -105,52 +80,49 @@ def ACAPP(A, epsilon = 0.1, verbose=False):
         norm_u = _np.linalg.norm(u)
         norm_v = _np.linalg.norm(v)
         sum_uv_norm_square += norm_u**2 * norm_v**2
+        # NEW PAPER ============
+        aux = 0
+        for index in range(k - 1):
+            aux += _np.vdot(u_vectors[index], u) * _np.vdot(v_vectors[index], v)
+        sum_uv_norm_square += 2 * _np.real(aux)
+        # ============ NEW PAPER
         error_rel = norm_u * norm_v / _np.sqrt(sum_uv_norm_square)
-        # error_rel = _np.linalg.norm(A-_np.asarray(u_vectors).T @ _np.asarray(v_vectors)) / _np.linalg.norm(A) # Estimación perfecta # TODO:
-        if error_rel <= epsilon or len(u_vectors) == min(m, n): # Epsilon reached or rank completed
+        condition = error_rel <= epsilon
+        # error_rel2 = norm_u * norm_v / (_np.linalg.norm(u_vectors[0]) * _np.linalg.norm(v_vectors[0]))
+        # error_rel3 = _np.linalg.norm(u) * _np.linalg.norm(v)
+        # condition = error_rel <= epsilon and error_rel2 <= epsilon and error_rel3 <= epsilon
+        if exact_error:
+            error_real = _np.linalg.norm(A - _np.asarray(u_vectors).T @ _np.asarray(v_vectors)) / norm_A # Estimación perfecta
+            condition = error_real <= epsilon
+        if condition or _np.sum(mask_col) == m or _np.sum(mask_row) == n: # or len(u_vectors) == min(m, n): # Epsilon reached or rank completed (checked entire matrix)
             # print("Reached Epsilon")
-            exit_code = 1
             break
 
-        # Otras formas de estimar: (se pueden agregar )
-        # TODO: si estimáramos mejor el error, no habría problemas en las matrices con bloques de ceros.
+        # Otras formas de estimar: (se pueden agregar)
         # 1) Literatura original: norm_u * norm_v <= epsilon
-        # 2) Literatura original: norm_u * norm_v / _np.linalg.norm(u_vectors[0]) * _np.linalg.norm(v_vectors[0]) <= epsilon
+        # 2) Literatura original: norm_u * norm_v / (_np.linalg.norm(u_vectors[0]) * _np.linalg.norm(v_vectors[0])) <= epsilon
         # 3) Literatura extra (al inicio del script):
         # for index in range(k - 1):
         #     sum_uv_norm_square += 2 * _np.abs(_np.dot(u_vectors[index], u)) * _np.abs(_np.dot(v_vectors[index], v))
 
-    if verbose:
-        # print(f"Finished in k={k} out of {m}")
-        # print(f"Relative error between matrices: {_np.linalg.norm(A-_np.asarray(u_vectors).T @ _np.asarray(v_vectors)) / _np.linalg.norm(A)}")
-    
-        print(f"Exit code: {exit_code}")
-        if len(u_vectors) > min(m, n):
-            print(">"*10)
-            print(f"Diff: {len(u_vectors) - min(m, n)}")
-            print(R.shape)
-            print(len(I), I)
-            u, c = _np.unique(I, return_counts=True)
-            print(u[c > 1], c[c > 1])
-            print(len(J), J)
-            u, c = _np.unique(J, return_counts=True)
-            print(u[c > 1], c[c > 1])
-            print(_np.linalg.norm(A-_np.asarray(u_vectors)[0:min(m, n),:].T @ _np.asarray(v_vectors)[0:min(m, n),:]) / _np.linalg.norm(A))
-            print(_np.linalg.norm(A-_np.asarray(u_vectors).T @ _np.asarray(v_vectors)) / _np.linalg.norm(A))
-            print("<"*10)
+    # Esto es para encontrar bloques que estén ocasionando un error muy grande:
+    # if _np.log10(_np.linalg.norm(A - _np.asarray(u_vectors).T @ _np.asarray(v_vectors)) / norm_A) - _np.log10(error_rel) >= 5:
+    #     import matplotlib.pyplot as plt
+    #     _np.save("A.npy", A)
+    #     print("Saved A!")
+    #     plt.spy(_np.abs(A))
+    #     plt.show()
 
     return _np.array(u_vectors), _np.array(v_vectors)
 
 def ACAPP_with_assembly(rows, cols, assembler, singular_matrix, epsilon = 0.1, verbose=False, dtype=_np.complex128):
-    rows = _np.asarray(rows)
-    cols = _np.asarray(cols)
+    # rows = _np.asarray(rows)
+    # cols = _np.asarray(cols)
 
     m, n = len(rows), len(cols)
     assembled_values = lil_matrix((m, n), dtype=dtype)
     mask_row = _np.zeros(n, dtype=bool)
     mask_col = _np.zeros(m, dtype=bool)
-    I = []
-    J = []
     k = 0
     i_star = 0
 
@@ -159,7 +131,6 @@ def ACAPP_with_assembly(rows, cols, assembler, singular_matrix, epsilon = 0.1, v
 
     # Stopping criterion:
     sum_uv_norm_square = 0
-    first_abs_delta = 0
     
     while True:
         aux = 0
@@ -168,8 +139,8 @@ def ACAPP_with_assembly(rows, cols, assembler, singular_matrix, epsilon = 0.1, v
         
         # Row of original matrix:
         if mask_col[i_star] != True and _np.sum(~mask_row) > 0:
-            assembled_values[i_star, ~mask_row] = assembler([rows[i_star]], cols[~mask_row], dtype=dtype) # PIN
-            # assembled_values[i_star:i_star+1, ~mask_row] = assembler([rows[i_star]], cols[~mask_row], dtype=dtype)
+            assembled_values[i_star, ~mask_row] = assembler(rows[i_star:i_star+1], cols[~mask_row], dtype=dtype) # PIN
+            # assembled_values[i_star:i_star+1, ~mask_row] = assembler(rows[i_star:i_star+1], cols[~mask_row], dtype=dtype)
             mask_col[i_star] = True
         R_row = assembled_values[i_star, :].toarray()
         # if singular_matrix is not None:
@@ -177,33 +148,30 @@ def ACAPP_with_assembly(rows, cols, assembler, singular_matrix, epsilon = 0.1, v
 
         R_row = R_row.flatten()
         R_row -= aux
-        R_row_copy = _np.copy(R_row)
-        R_row_copy[J] = 0
-        j_star = _np.argmax(_np.abs(R_row_copy))
-        delta = R_row_copy[j_star]
-        if first_abs_delta == 0:
-            first_abs_delta = abs(delta)
+        R_row_abs = _np.abs(R_row)
+        R_row_abs[mask_row] = -1
+        j_star = _np.argmax(R_row_abs)
+        assert R_row_abs[j_star] != -1, "-1 found in R_row_abs"
+        if R_row_abs[j_star] == -1:
+            delta = 0
+        else:
+            delta = R_row[j_star]
 
-        # if delta == 0 or abs(delta)/first_abs_delta <= 1e-15: # Agregamos un pequeño margen (puede ser complejo)
         if delta == 0:
-            if _np.sum(mask_col) >= min(m, n):
+            if _np.sum(mask_col) == m: # OJO: acá tenía min(m,n), pero eso no es correcto
                 if len(u_vectors) == 0:
-                    u_vectors.append(_np.zeros(m, dtype=R_row.dtype)) # 1xm
-                    v_vectors.append(_np.zeros(n, dtype=R_row.dtype)) # 1xn
+                    if m + n <= m * n:
+                        u_vectors.append(_np.zeros(m, dtype=R_row.dtype)) # 1xm
+                        v_vectors.append(_np.zeros(n, dtype=R_row.dtype)) # 1xn
+                    else:
+                        matrix = assembled_values.toarray()
+                        if _np.sum(~mask_col) > 0 and _np.sum(~mask_row) > 0:
+                            meshgrid = _np.meshgrid(_np.arange(m)[~mask_col], _np.arange(n)[~mask_row], indexing="ij")
+                            matrix[meshgrid[0], meshgrid[1]] = assembler(rows[~mask_col], cols[~mask_row], dtype=dtype)
+                        return matrix, None
                 break
             i_star = _np.random.choice(_np.arange(m)[~mask_col])
             continue
-            # ===========
-            # if len(I) >= min(m, n): # Para que no siga agregando en caso de ya tener todos los índices
-            #     # print("Not reached Epsilon")
-            #     break
-            # if len(u_vectors) == 0: # Edge case in which the first row chosen consists of zeros only
-            #     i_star += 1
-            #     if i_star == m: # Everything is zero
-            #         u_vectors.append(_np.zeros(m, dtype=R_row.dtype)) # 1xm
-            #         v_vectors.append(_np.zeros(n, dtype=R_row.dtype)) # 1xn
-            #         break
-            #     continue
         else:
             v = R_row / delta
             aux = 0
@@ -212,8 +180,8 @@ def ACAPP_with_assembly(rows, cols, assembler, singular_matrix, epsilon = 0.1, v
             
             # Column of original matrix:
             if mask_row[j_star] != True and _np.sum(~mask_col) > 0:
-                assembled_values[~mask_col, j_star] = assembler(rows[~mask_col], [cols[j_star]], dtype=dtype) # PIN
-                # assembled_values[~mask_col, j_star:j_star+1] = assembler(rows[~mask_col], [cols[j_star]], dtype=dtype)
+                assembled_values[~mask_col, j_star] = assembler(rows[~mask_col], cols[j_star:j_star+1], dtype=dtype) # PIN
+                # assembled_values[~mask_col, j_star:j_star+1] = assembler(rows[~mask_col], cols[j_star:j_star+1], dtype=dtype)
                 mask_row[j_star] = True
             u = assembled_values[:, j_star].toarray()
             # if singular_matrix is not None:
@@ -224,14 +192,12 @@ def ACAPP_with_assembly(rows, cols, assembler, singular_matrix, epsilon = 0.1, v
             k += 1
             u_vectors.append(u)
             v_vectors.append(v)
-        I.append(i_star)
-        J.append(j_star)
-        u_copy = _np.copy(u_vectors[-1])
-        u_copy[I] = 0
-        i_star = _np.argmax(_np.abs(u_copy))
+        u_abs = _np.abs(u_vectors[-1])
+        u_abs[mask_col] = -1
+        i_star = _np.argmax(u_abs)
 
         # Check if compression is still viable:
-        if len(u_vectors) * len(u_vectors[0]) + len(v_vectors) * len(v_vectors[0]) > m * n:
+        if len(u_vectors) * m + len(v_vectors) * n > m * n:
             matrix = assembled_values.toarray()
             if _np.sum(~mask_col) > 0 and _np.sum(~mask_row) > 0:
                 meshgrid = _np.meshgrid(_np.arange(m)[~mask_col], _np.arange(n)[~mask_row], indexing="ij")
@@ -245,7 +211,7 @@ def ACAPP_with_assembly(rows, cols, assembler, singular_matrix, epsilon = 0.1, v
         norm_v = _np.linalg.norm(v)
         sum_uv_norm_square += norm_u**2 * norm_v**2
         error_rel = norm_u * norm_v / _np.sqrt(sum_uv_norm_square)
-        if error_rel <= epsilon or len(u_vectors) == min(m, n): # Epsilon reached or rank completed
+        if error_rel <= epsilon or _np.sum(mask_col) == m or _np.sum(mask_row) == n: # Epsilon reached or rank completed (checked entire matrix)
             # print("Reached Epsilon")
             break
 
--- MyHM/numba/aca.py
+++ MyHM/numba/aca.py
@@ -1,13 +1,12 @@
-import numpy as np
+import numpy as _np
 from numba import njit
 
 @njit()
-def ACAPP_numba(rows, cols, info, numba_assembler, epsilon=0.1, dtype=np.complex128):
+def ACAPP_numba(rows, cols, info, numba_assembler, epsilon=0.1, dtype=_np.complex128):
+    '''Standard ACA with partial pivoting'''
     m, n = len(rows), len(cols)
-    mask_row = np.zeros(n, dtype=np.bool_) # OJO
-    mask_col = np.zeros(m, dtype=np.bool_) # OJO
-    I = []
-    J = []
+    mask_row = _np.zeros(n, dtype=_np.bool_) # OJO
+    mask_col = _np.zeros(m, dtype=_np.bool_) # OJO
     k = 0
     i_star = 0
 
@@ -19,31 +18,31 @@ def ACAPP_numba(rows, cols, info, numba_assembler, epsilon=0.1, dtype=np.complex
     
     while True:
         # Row of original matrix:
-        if mask_col[i_star] != True and np.sum(~mask_row) > 0:
-            mask_col[i_star] = True
         R_row = numba_assembler(rows[i_star:i_star+1], cols, info, dtype).flatten()
+        mask_col[i_star] = True
 
-        aux = np.zeros_like(R_row) # OJO: no deja =0 directamente
+        aux = _np.zeros_like(R_row) # OJO: no deja =0 directamente
         # if k > 0:
-        #     aux = np.array(u_vectors)[:, i_star].T @ np.array(v_vectors)
+        #     aux = _np.array(u_vectors)[:, i_star].T @ _np.array(v_vectors)
         for l in range(k): # OJO: lo de arriba no le gusta
             aux += u_vectors[l][i_star] * v_vectors[l]
-
         R_row -= aux
-        R_row_copy = np.copy(R_row)
-        # R_row_copy[np.array(J)] = 0 # OJO: no me deja sin np.array
-        R_row_copy[mask_row] = 0 # OJO: alternativa al anterior
-        j_star = np.argmax(np.abs(R_row_copy))
-        delta = R_row_copy[j_star]
 
-        # if delta == 0 or abs(delta)/first_abs_delta <= 1e-15: # Agregamos un pequeño margen (puede ser complejo)
+        R_row_abs = _np.abs(R_row)
+        R_row_abs[mask_row] = -1 
+        j_star = _np.argmax(R_row_abs)
+        assert R_row_abs[j_star] != -1, "-1 found in R_row_abs"
+        if R_row_abs[j_star] == -1: # En teoría esto nunca debería ocurrir (por la condición de salida más abajo)
+            delta = 0
+        else:
+            delta = R_row[j_star]
+
         if delta == 0:
-            # if np.sum(mask_col) >= min(m, n):
-            if np.sum(mask_col) == m: # # OJO: acá tenía min(m,n), pero eso no es correcto
+            if _np.sum(mask_col) == m: # OJO: acá tenía min(m,n), pero eso no es correcto
                 if len(u_vectors) == 0:
                     if m + n <= m * n:
-                        u_vectors.append(np.zeros(m, dtype=R_row.dtype)) # 1xm
-                        v_vectors.append(np.zeros(n, dtype=R_row.dtype)) # 1xn
+                        u_vectors.append(_np.zeros(m, dtype=R_row.dtype)) # 1xm
+                        v_vectors.append(_np.zeros(n, dtype=R_row.dtype)) # 1xn
                     else: # En caso de que la compresión use más espacio que el bloque completo
                         # # Versión retornando listas:
                         # v_vectors.clear()
@@ -53,39 +52,34 @@ def ACAPP_numba(rows, cols, info, numba_assembler, epsilon=0.1, dtype=np.complex
 
                         # Versión retornando arrays:
                         array_u = numba_assembler(rows, cols, info, dtype)
-                        # array_v = np.array([[0]], dtype=R_row.dtype)
-                        array_v = np.array([()], dtype=R_row.dtype)
+                        array_v = _np.array([()], dtype=R_row.dtype)
                         return array_u, array_v
                 break
-            i_star = np.random.choice(np.arange(m)[~mask_col])
+            i_star = _np.random.choice(_np.arange(m)[~mask_col]) # Quizás se puede hacer un arange afuera en vez de crear uno cada vez acá?
             continue
         else:
             v = R_row / delta
 
             # Column of original matrix:
-            if mask_row[j_star] != True and np.sum(~mask_col) > 0:
-                mask_row[j_star] = True
             u = numba_assembler(rows, cols[j_star:j_star+1], info, dtype).flatten()
+            mask_row[j_star] = True
 
-            aux = np.zeros_like(u) # OJO: no deja =0 directamente
+            aux = _np.zeros_like(u) # OJO: no deja =0 directamente
             # if k>0:
-            #     aux = np.asarray(v_vectors)[:, j_star].T @ np.asarray(u_vectors)
+            #     aux = _np.asarray(v_vectors)[:, j_star].T @ _np.asarray(u_vectors)
             for l in range(k): # OJO: lo de arriba no le gusta
                 aux += v_vectors[l][j_star] * u_vectors[l]
-
             u -= aux
+
             k += 1
             u_vectors.append(u)
             v_vectors.append(v)
-        I.append(i_star)
-        J.append(j_star)
-        u_copy = np.copy(u_vectors[-1])
-        # u_copy[np.array(I)] = 0 # OJO: no me deja sin np.array
-        u_copy[mask_col] = 0 # OJO: alternativa al anterior
-        i_star = np.argmax(np.abs(u_copy))
+        u_abs = _np.abs(u_vectors[-1])
+        u_abs[mask_col] = -1 # OJO: alternativa al anterior
+        i_star = _np.argmax(u_abs)
 
-        # # Check if compression is still viable:
-        if len(u_vectors) * len(u_vectors[0]) + len(v_vectors) * len(v_vectors[0]) > m * n:
+        # Check if compression is still viable:
+        if len(u_vectors) * m + len(v_vectors) * n > m * n:
             # # Versión retornando listas:
             # v_vectors.clear()
             # matrix = numba_assembler(rows, cols, info, dtype)
@@ -94,27 +88,32 @@ def ACAPP_numba(rows, cols, info, numba_assembler, epsilon=0.1, dtype=np.complex
 
             # Versión retornando arrays:
             array_u = numba_assembler(rows, cols, info, dtype)
-            # array_v = np.array([[0]], dtype=R_row.dtype)
-            array_v = np.array([()], dtype=R_row.dtype)
+            array_v = _np.array([()], dtype=R_row.dtype)
             return array_u, array_v
 
         # Stopping criterion:
         u = u_vectors[-1]
         v = v_vectors[-1]
-        norm_u = np.linalg.norm(u)
-        norm_v = np.linalg.norm(v)
+        norm_u = _np.linalg.norm(u)
+        norm_v = _np.linalg.norm(v)
         sum_uv_norm_square += norm_u**2 * norm_v**2
-        error_rel = norm_u * norm_v / np.sqrt(sum_uv_norm_square)
-        if error_rel <= epsilon or len(u_vectors) == min(m, n): # Epsilon reached or rank completed
+        # NEW PAPER ============
+        aux = 0
+        for i in range(k - 1):
+            aux += _np.vdot(u_vectors[i], u) * _np.vdot(v_vectors[i], v)
+        sum_uv_norm_square += 2 * _np.real(aux)
+        # ============ NEW PAPER
+        error_rel = norm_u * norm_v / _np.sqrt(sum_uv_norm_square)
+        if error_rel <= epsilon or _np.sum(mask_col) == m or _np.sum(mask_row) == n: # Epsilon reached or rank completed (checked entire matrix)
             # print("Reached Epsilon")
             break
 
     # # Versión retornando listas:
-    # return u_vectors, v_vectors # Así que me tengo que acordar de transformarlos a np.arrays cuando los reciba en el árbol!
+    # return u_vectors, v_vectors # Con este me tengo que acordar de transformarlos a _np.arrays cuando los reciba en el árbol!
 
     # Versión retornando arrays:
-    array_u = np.empty((len(u_vectors), m), dtype=R_row.dtype)
-    array_v = np.empty((len(v_vectors), n), dtype=R_row.dtype)
+    array_u = _np.empty((len(u_vectors), m), dtype=R_row.dtype)
+    array_v = _np.empty((len(v_vectors), n), dtype=R_row.dtype)
     for i in range(len(u_vectors)):
         array_u[i] = u_vectors[i]
         array_v[i] = v_vectors[i]
